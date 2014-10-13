5af530ca31707614ed209665409ffb8012819de1
thanhtnguyen <thanhtnguyen@paypal.com>
2014-10-13 16:10:36 +0800

    Add comment & minor fixes

--- clicker.rb
+++ clicker.rb
@@ -13,16 +13,25 @@ Capybara.default_wait_time = 10
 
 describe 'Travel Smart clicker', :type => :feature do
   it 'click until no point left' do
+    # Login to website
+    Capybara.reset_session!
     puts 'Go to website'
     visit 'https://www.travelsmartrewards.sg/'
+
+    # Fill the login form
     expect(page).to have_content("Incentives for Singapore's Commuters")
     fill_in 'Email', with: EMAIL_ADDRESS
     fill_in 'Password', with: PASSWORD
     click_on 'Login'
 
+    # Check to make sure we are on the correct page
     expect(page).to have_content('Spin to Win')
+
+    # Start the game!
     find('#wgt-game').click
     sleep 5
+
+    # Now the popup should shows, all left is to click
     within_frame find('.fancybox-iframe')[:id] do
       nturn = find('#points').native.text.to_i / 10
       nturn.times do
